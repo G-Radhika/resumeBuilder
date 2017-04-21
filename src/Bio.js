caa6135c7acc0a9345aee39dcb27112d069c779b
Radhika Godugu <radhik.g@gmail.com>
2017-04-21 10:39:02 -0700

    create-react-app commit

---
+++ src/Bio.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Bio extends Component {
+  render() {
+    return (
+        <div className="bla">
+         This is Bio Component
+        </div>
+    );
+  }
+}
+
+export default Bio;
